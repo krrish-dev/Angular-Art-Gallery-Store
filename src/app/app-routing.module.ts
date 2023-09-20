0ac14abb1c81d1f607ac51e1d3ff146eb6a2e528
krrish_dev <justbegintoday@gmail.com>
2023-09-21 02:46:28 +0300

    lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './website/home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { CartComponent } from './website/cart/cart.component'; // Import the CartComponent
-import { CheckoutComponent } from './website/checkout/checkout.component';
-import { ProductDetailsComponent } from './website/product-details/product-details.component';
-import { MyOrdersComponent } from './website/my-orders/my-orders.component';
-import { AuthGuard } from './guards/auth.guard';
-import { MyProfileComponent } from './website/my-profile/my-profile.component';
+// import { CartComponent } from './website/cart/cart.component'; // Import the CartComponent
+// import { CheckoutComponent } from './website/checkout/checkout.component';
+// import { ProductDetailsComponent } from './website/product-details/product-details.component';
+// import { MyOrdersComponent } from './website/my-orders/my-orders.component';
+// import { AuthGuard } from './guards/auth.guard';
+// import { MyProfileComponent } from './website/my-profile/my-profile.component';
 
 
 const routes: Routes = [
@@ -16,18 +16,22 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
-  { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
-  { path: 'product/:id', component: ProductDetailsComponent},
-  { path: 'my-orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-  { path: 'my-profile', component: MyProfileComponent, canActivate: [AuthGuard] },
+  // { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
+  // { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
+  // { path: 'product/:id', component: ProductDetailsComponent},
+  // { path: 'my-orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+  // { path: 'my-profile', component: MyProfileComponent, canActivate: [AuthGuard] },
 
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin-routing.module').then((m) => m.AdminRoutingModule),
 
   },
-
+  {
+    path: 'website',
+    loadChildren: () =>
+      import('./website/website.module').then((m) => m.WebsitePagesModule),
+  },
 ];
 
 @NgModule({
